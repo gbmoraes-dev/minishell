7aac90c1100e7a43e6a8e4f3898fde2881c1261b
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-10-31 16:54:20 -0300

    feat: implement signal handling for SIGINT and SIGQUIT

---
+++ src/signals/signal.c
@@ -0,0 +1,31 @@
+#include "../../include/minishell.h"
+
+void	signals_handler(void)
+{
+	struct sigaction	sig;
+
+	signal(SIGQUIT, SIG_IGN);
+	sig.sa_handler = sigint_handler;
+	sigemptyset(&sig.sa_mask);
+	sigaddset(&sig.sa_mask, SIGINT);
+	sig.sa_flags = 0;
+	sigaction(SIGINT, &sig, NULL);
+	return ;
+}
+
+void	sigint_handler(int signal)
+{
+	extern unsigned int	g_exit_status;
+
+	if (signal == SIGINT)
+	{
+		if (RL_ISSTATE(RL_STATE_READCMD))
+			ioctl(STDIN_FILENO, TIOCSTI, "\n");
+		else
+			write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 1);
+		rl_on_new_line();
+		g_exit_status = 1;
+	}
+	return ;
+}
