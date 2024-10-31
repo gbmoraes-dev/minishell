9398668d2a5ff4ba98ef2acc1754c081ce0656fe
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-10-31 19:59:59 -0300

    feat: add functions to find and remove environment variables in linked list

--- src/list/env_lst.c
+++ src/list/env_lst.c
@@ -47,3 +47,53 @@ char	*find_env_in_lst(t_list *env_node, char *env)
 		line = ft_strchr(line, '=') + 1;
 	return (line);
 }
+
+char	**find_env_node(t_list *env_node, char *env)
+{
+	char	*key;
+	char	*node_content;
+	char	**match;
+
+	match = NULL;
+	if (!env_node || !env)
+		return (NULL);
+	while (env_node)
+	{
+		node_content = env_node->content;
+		key = ft_substr(node_content, 0, ft_strchr(node_content, '=')
+				- node_content);
+		if (!ft_strncmp(key, env, ft_strlen(key) + 1))
+		{
+			match = (char **)&env_node->content;
+			free(key);
+			break ;
+		}
+		free(key);
+		env_node = env_node->next;
+	}
+	return (match);
+}
+
+void	remove_env_in_lst(t_list **lst, char *key)
+{
+	char	*current_key;
+	t_list	*current_node;
+
+	if (!lst || !*lst)
+		return ;
+	current_node = *lst;
+	current_key = ft_substr(current_node->content, 0,
+			ft_strchr(current_node->content, '=') - current_node->content);
+	if (!ft_strncmp(key, current_key, ft_strlen(current_key) + 1))
+	{
+		*lst = current_node->next;
+		free(current_node->content);
+		free(current_node);
+		free(current_key);
+		return ;
+	}
+	else
+		remove_env_in_lst(&current_node->next, key);
+	free(current_key);
+	return ;
+}
