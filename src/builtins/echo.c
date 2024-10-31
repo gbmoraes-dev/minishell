0be69b8c8ffebbdc7bcebfba3a6b2ebb769b871a
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-10-31 20:00:55 -0300

    feat: implement echo for echoing text with optional newline

---
+++ src/builtins/echo.c
@@ -0,0 +1,29 @@
+#include "../../include/minishell.h"
+
+int	my_echo(t_token *token)
+{
+	int		should_print_new_line;
+	t_token	*head;
+
+	should_print_new_line = 1;
+	head = token;
+	if (token->next)
+	{
+		if (!ft_strncmp(token->next->content, "-n", 2))
+		{
+			should_print_new_line = 0;
+			token = token->next;
+		}
+		token = token->next;
+		while (token && token->type != PIPE)
+		{
+			ft_putstr_fd(token->content, head->fd_out);
+			token = token->next;
+			if (token && token->type != PIPE)
+				ft_putchar_fd(' ', head->fd_out);
+		}
+	}
+	if (should_print_new_line)
+		ft_putchar_fd('\n', head->fd_out);
+	return (0);
+}
